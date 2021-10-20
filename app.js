49c63719f02bd102cd1cba07c53d976bc1aa4c8e
xfrogz <renattoyague@gmail.com>
2021-10-20 01:59:38 -0500

    My contribution

--- app.js
+++ app.js
@@ -44,15 +44,16 @@ document.addEventListener("DOMContentLoaded", function () {
   if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
+// What's app contact
 
-
-// What's app contact  
-
-var frm = document.querySelector('#contact')
+var frm = document.querySelector("#contact");
 frm.addEventListener("submit", (err) => {
   // Prevents from refresh
   err.preventDefault();
   var name = document.getElementById("name").value;
   var messege = document.getElementById("messege").value;
-  var win = window.open(`https://wa.me/+919041063244?text=Hello%2C%20my%20name%20is%20%20${name},%20${messege}`, '_blank');
+  var win = window.open(
+    `https://wa.me/+919041063244?text=Hello%2C%20my%20name%20is%20%20${name},%20${messege}`,
+    "_blank"
+  );
 });
